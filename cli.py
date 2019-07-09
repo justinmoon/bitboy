11872beaa391a3fb377e4005153fca4ddb7dcccd
Justin Moon <mail@justinmoon.com>
2019-07-09 18:19:08 -0500

    it worked once

--- cli.py
+++ cli.py
@@ -2,29 +2,29 @@ import sys
 import time
 import json
 import subprocess
+import argparse
 
 from pprint import pprint
 
 import serial
+from serial.tools import list_ports
 
 
-port = sys.argv[1]
-ser =  serial.Serial(port, baudrate=115200)
-
-
-def sign(tx, script_pubkey):
+def sign(tx, script_pubkey=None, redeem_script=None):
     command = "sign"
     msg = {
         "command": command,
         "payload": {
             "tx": tx,
             "script_pubkey": script_pubkey,
+            # "redeem_script": redeem_script,
         }
     }
     ser.write(json.dumps(msg).encode() + b'\r\n')
     # wallet response
     while True:
         raw_res = ser.read_until(b'\r\n').strip(b'\r\n').decode()
+        print(raw_res)
         try:
             res = json.loads(raw_res)
         except:
@@ -38,7 +38,6 @@ def sign(tx, script_pubkey):
         return res['signed']
  
 def rpc(*args, returns_json=True):
-    print(args)
     res = subprocess.run(['bitcoin-cli', '-testnet'] + list(args), 
                           capture_output=True)
     if not res.stderr:
@@ -49,44 +48,98 @@ def rpc(*args, returns_json=True):
     else:
         raise Exception(res.stderr)
 
-def main():
-    # show them their unspents
+def find_port():
+    ports = list(list_ports.grep('CP2104 USB to UART Bridge Controller'))
+    if len(ports) > 1:
+        raise OSError("too many devices plugged in")
+    elif len(ports) == 0:
+        raise OSError("no device plugged in")
+    else:
+        return ports[0].device
+
+def parse():
+    parser = argparse.ArgumentParser(description='BitBoy Bitcoin Wallet')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # "bitboy send"
+    send = subparsers.add_parser('send', help='send bitcoins')
+    send.add_argument('recipient', help='bitcoin address')
+    send.add_argument('amount', help='how many bitcoins to send')
+    send.add_argument('coin', help='txid:index of output to spend')
+    send.set_defaults(func=handle_send)
+
+    # "bitboy receive"
+    receive = subparsers.add_parser('receive', help='receive bitcoins')
+
+    # "bitboy balance"
+    balance = subparsers.add_parser('balance', help='count your sats')
+
+    # "bitboy history"
+    history = subparsers.add_parser('history', help='your transactions')
+
+    # "bitboy unspent"
+    unspent = subparsers.add_parser('unspent', help='your utxos')
+    unspent.set_defaults(func=handle_unspent)
+
+    return parser.parse_args()
+
+
+def handle_unspent(args):
     unspent = rpc('listunspent')
     pprint(unspent)
 
-    # let them select one by pasting txid
-    txid, vout = input("txid:vout to spend: ").split(":")
-    # txid, vout = "0fae469f3d3c546c3d57f612c82eefc153648c9ce9adda69082258fd0a740d7c:0".split(":")
-    tx = rpc('gettransaction', txid)
 
-    # collect recipient
-    recipient_addr = input("address of recipient: ")
-    # recipient_addr = "2MsvWeJqUG9QQwCDJ16q4iJhjybqAeK33tL"
-
-    # construct unsigned transaction
+def handle_send(args):
+    # construct inputs
+    input_id, input_index = args.coin.split(':')
+    parent = rpc('getrawtransaction', input_id, "true")
+    utxo = parent['vout'][int(input_index)]
     inputs = json.dumps([{
-        "txid": txid,
-        "vout": int(vout),
+        "txid": input_id,
+        "vout": int(input_index),
     }])
+
+    # construct outputs
     outputs = json.dumps([{
-        recipient_addr: str(round(float(tx['amount']) - 0.0001, 9))
+        args.recipient: args.amount
     }])
+
+    #  have bitcoind construct transaction
     tx = rpc('createrawtransaction', inputs, outputs, returns_json=False)
     tx = tx.strip().decode()
+    # print(tx)
 
-    # get scriptPubKey
+    # gather metadata required by signature
+    redeem_script = script_pubkey = witness = None
+    txtype = utxo["scriptPubKey"]["type"]
+
+    # FIXME
+    # if txtype == "pubkeyhash":
+        # script_pubkey = utxo["scriptPubKey"]["hex"]
+    # elif txtype == 'scripthash':
+        # redeem_script = utxo["scriptPubKey"]["hex"]
+    # else:
+        # raise NotImplementedError()
+    unspent = rpc('listunspent', '0')
     for u in unspent:
-        if u['txid'] == txid:
-            print(u)
-            script_pubkey = u['scriptPubKey']
+        if u['txid'] == input_id and u['vout'] == int(input_index):
+            if txtype == "pubkeyhash":
+                script_pubkey = u["scriptPubKey"]
+            elif txtype == 'scripthash':
+                redeem_script = u["redeemScript"]
             break
 
-    signed = sign(tx, script_pubkey)
-    print("signed", signed)
+    assert script_pubkey or redeem_script
 
+
+    # sign & broadcast 
+    signed = sign(tx, script_pubkey=script_pubkey, redeem_script=redeem_script)
     sent = rpc('sendrawtransaction', signed, returns_json=False)
     print(sent)
 
+ser =  serial.Serial(find_port(), baudrate=115200)
 
 if __name__ == '__main__':
-    main()
+    args = parse()
+    args.func(args)
+
--- p2pkh.py
+++ p2pkh.py
@@ -20,46 +20,69 @@ lcd.erase()
 def encode_script(script_bytes):
     return encode_varint(len(script_bytes)) + script_bytes
 
+def script_from_hex(s):
+    sbytes = unhexlify(s)
+    ebytes = encode_script(sbytes)  # FIXME
+    script = Script.parse(BytesIO(ebytes))
+    print("script:", script)
+    return script
+
+
+def handle_msg(msg):
+    if msg["command"] == "sign":
+        print("signing")
+        # parse transaction
+        tx = Tx.parse(BytesIO(unhexlify(msg['payload']['tx'])), testnet=True)
+        # parse script pubkey / redeem script
+        if msg['payload']['script_pubkey']:
+            script_pubkey = script_from_hex(msg['payload']['script_pubkey'])
+            redeem_script = None
+        # elif msg['payload']['redeem_script']:
+            # script_pubkey = None
+            # redeem_script = script_from_hex(msg['payload']['redeem_script'])
+        # else:
+            # raise ValueError("bad message: {}".format(msg))
+        redeem_script = None
+        signed = sign_p2pkh(tx, script_pubkey=script_pubkey, redeem_script=redeem_script)
+        res = {
+            "signed": signed,
+        }
+        print(json.dumps(res))  # send to cli
+        return json.dumps(res)
+    else:
+        print("not signing")
+
+
 def reader():
     while True:
         data = input()
+        # raise
+        # print("received msg:", data)
         try:
             msg = json.loads(data)
-        except json.JSONDecodeError as e:
+            print('json read:', msg)
+        except Exception as e:
             print('json error', e)
             continue
-        if msg["command"] == "sign":
-            print("signing")
-            signed = sign_p2pkh(msg["payload"]["tx"], msg["payload"]["script_pubkey"])
-            res = {
-                "signed": signed,
-            }
-            print(json.dumps(res))
-        else:
-            print("not signing")
+        handle_msg(msg)
 
 
-def sign_p2pkh(tx_hex, script_pubkey_hex):
-# def sign_p2pkh():
+def sign_p2pkh(tx, script_pubkey=None, redeem_script=None):
     """only supports 1 input from hard-coded private key for now"""
     # hard-coded secret
     secret = 58800187338825965989061197411175755305019286370732616970021105328088303800804
     key = PrivateKey(secret)
     
-    # parse transaction
-    tx = Tx.parse(BytesIO(unhexlify(tx_hex)), testnet=True)
-
-    # parse script pubkey
-    script_pubkey_bytes = unhexlify(script_pubkey_hex)
-    encoded_script_pubkey = encode_script(script_pubkey_bytes)  # FIXME
-    script_pubkey = Script.parse(BytesIO(encoded_script_pubkey))
-
     # sign first input
     assert len(tx.tx_ins) == 1
-    tx.sign_input(0, key, script_pubkey=script_pubkey)
+    tx.sign_input(0, key, script_pubkey=script_pubkey, redeem_script=redeem_script)
 
     # return hex-serialized signed transaction
     return hexlify(tx.serialize())
 
 
 reader()
+
+def test():
+    msg = {'command': 'sign', 'payload': {'tx': '020000000181a093ad29afafa839b3914621be1b0df5772c140d25eb711343ab64fd35a13d0100000000ffffffff01301b0f000000000017a914076e203ae0877b2de7ead90c04532b881e7145318700000000', 'script_pubkey': 'a91422262ea1f6770b2cc5d7d6b3c1af80b477bfa1bb87', 'redeem_script': None}}
+    print(handle_msg(msg))
