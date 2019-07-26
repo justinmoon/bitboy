71eb634a702e1ecbad955f6775634f59bbe49c54
Justin Moon <mail@justinmoon.com>
2019-07-26 03:26:21 -0500

    generate mnemonic, save / load master key

--- bitboy/main.py
+++ bitboy/main.py
@@ -2,7 +2,7 @@
 steps:
 - [x] asyncio button task that prints "Button N" to screen
 - [x] asyncio serial task that prints messages to screen (2 tasks at once)
-- screen for traversing HD hierarchy
+- [x] screen for traversing HD hierarchy
 - mnemonic screen
 - signing
 '''
@@ -16,6 +16,8 @@ from m5stack.pins import BUTTON_A_PIN, BUTTON_B_PIN, BUTTON_C_PIN
 from m5stack import LCD, fonts
 from sys import stdin, stdout
 from bitcoin.hd import HDPrivateKey
+from bitcoin.mnemonic import secure_mnemonic
+import urandom
 
 A_PIN = Pin(BUTTON_A_PIN, Pin.IN, Pin.PULL_UP)
 B_PIN = Pin(BUTTON_B_PIN, Pin.IN, Pin.PULL_UP)
@@ -70,9 +72,7 @@ def traverse_button(pin):
 
 
 async def button_manager():
-    # TODO: these will need to be accessible from serial coroutine.
-    # basically, global variables so that new behavior can be registered w/ .x_func()
-    # and i'm not sure why this must run in a coroutine ...
+    # TODO: i'm not sure why this must run in a coroutine ...
     A_BUTTON.release_func(traverse_button, (A_PIN,))
     B_BUTTON.release_func(traverse_button, (B_PIN,))
     C_BUTTON.release_func(traverse_button, (C_PIN,))
@@ -86,15 +86,63 @@ async def serial_manager():
         await swriter.awrite(res)
         lcd.print(res)
 
+def title(s):
+    # calculations
+    sw = fonts.tt32.get_width(s)
+    padding = (lcd.width - sw) // 2
+
+    # configure lcd
+    lcd.set_font(fonts.tt32)
+    lcd.set_pos(padding, 20)
+
+    # print
+    lcd.print(s)
+
+def mnemonic_columns(mnemonic):
+    # print title
+    title("Seed Words")
+
+    # set font
+    lcd.set_font(fonts.tt24)
+
+    # variables for printing
+    words = mnemonic.split()
+    labeled = [str(i) + ". " + word for i, word in enumerate(words, 1)]
+    words_per_col = len(words) // 2
+    col_width = max([lcd._font.get_width(w) for w in labeled])
+    # 2 colunms with equal spacing on all sides
+    pad_x = (lcd.width - 2 * col_width) // 3
+    pad_y = 20
+    left_col_x, left_col_y = pad_x, lcd._y + pad_y
+    right_col_x, right_col_y = 2 * pad_x + col_width, lcd._y + pad_y
+
+    # print left column
+    lcd.set_pos(left_col_x, left_col_y)
+    for word in labeled[:words_per_col]:
+        lcd.print(word)
+
+    # print right column
+    lcd.set_pos(right_col_x, right_col_y)
+    for word in labeled[words_per_col:]:
+        lcd.print(word)
+
 async def start():
     global KEY
-    lcd.print('Deriving')
-    mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
-    password = ""
-    path = b"m/84'/1'/0'"
-    KEY = HDPrivateKey.from_mnemonic(mnemonic, password, path=path, testnet=True)
-    child = KEY.child(0, False)
-    print_address(child)
+
+    # attempt to load KEY from filesystem, otherwise generate and print mnemonic
+    try:
+        with open('key', 'rb') as f:
+            KEY = HDPrivateKey.parse(f)
+            child = KEY.child(0, False)
+            print_address(child)
+    except:
+        mnemonic = secure_mnemonic()
+        mnemonic_columns(mnemonic)
+        password = ""
+        path = b"m/84'/1'/0'"
+        KEY = HDPrivateKey.from_mnemonic(mnemonic, password, path=path, testnet=True)
+        with open("key", "wb") as f:
+            f.write(KEY.serialize())
 
 def main():
     loop = uasyncio.get_event_loop()
